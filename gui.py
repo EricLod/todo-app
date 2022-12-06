900d2a7d5af2c457d93f2aa1201fde3235659634
EricLod <eric.wiehman@lodicon.co.za>
2022-12-06 15:22:31 +0200

    Successful GUI add

--- gui.py
+++ gui.py
@@ -4,6 +4,7 @@ import PySimpleGUI as sg
 label = sg.Text("Hello World")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
+list_box = sg.ListBox(values=["Buy groceries"])
 
 window = sg.Window("My To-Do App",
                    layout=[[label], [input_box, add_button]],
